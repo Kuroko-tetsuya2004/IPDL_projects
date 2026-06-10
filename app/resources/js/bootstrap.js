4bc5e39aead2ff3eb694ce067001e5bb65bea240
Kuroko-tetsuya2004 <wadem079@gmail.com>
2026-06-10 08:29:38 +0000

    Update bootstrap.js

--- app/resources/js/bootstrap.js
+++ app/resources/js/bootstrap.js
@@ -1,3 +1,5 @@
 import axios from 'axios'
 window.axios = axios
+
+window.axios.defaults.withCredentials = true  // ✅ AJOUT
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest'
